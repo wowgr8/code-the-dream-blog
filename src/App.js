71985e5076b2174e28a832fbe2e14771af7a25a4
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-20 18:31:28 -0500

    add list array holding objects with title, url, author, num_comments, points, and objectID properties. Replace create-react-app default code with h1 title, search field and placeholder for list array display.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+    <hr />
+      {/* render the list here */}
+    </div> 
   );
 }
 
