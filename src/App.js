29f28c795ab80f6164555febd9ac8893581ca0f7
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-22 22:08:59 -0500

    add refactored App, List, and Search functions - arrow notation

--- src/App.js
+++ src/App.js
@@ -19,50 +19,42 @@ const list = [
   }
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <Search />
+    <Search />
 
-      <hr />
-      
-      {/* creating an instance of List component */} 
-      <List />
-      {/* creating another instance of List component */} 
-      <List />
-    </div> 
-  );
-}
+    <hr />
+    
+    {/* creating an instance of List component */} 
+    <List />
+    {/* creating another instance of List component */} 
+    <List />
+  </div> 
+);
 
-function Search() {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div> 
-  );
-}
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div> 
+);
 
+const List = () => (
+  <ul>
+    {list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li> 
+      ))}
+  </ul>
+); 
 
-function List() {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li> 
-        );  
-      })}
-    </ul>
-  ); 
-}
 
 export default App;
