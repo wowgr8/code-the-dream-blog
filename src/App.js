2a067cf09ebae92a99aa1e253698333db58cda6a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-26 23:49:24 -0500

    add event.target.value to handleChange function.

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ const App = () => (
 
 const Search = () => {
   const handleChange = (event) => {
-    console.log(event);
+    console.log(event.target.value);
   };
-  
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
