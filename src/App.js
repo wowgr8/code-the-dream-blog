a8da636956a51a48a70f28b306dae7deac80eb8c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-27 23:41:31 -0500

    add block body to app component function, rename list array to stories and moved it inside app component.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, 
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    }, 
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ];
+  
+  return(
+    <div>
+      <h1>My Hacker Stories</h1>
 
-    <Search />
+      <Search />
 
-    <hr />
+      <hr />
 
-    <List />
-  </div> 
-);
+      <List />
+    </div> 
+  )
+};
 
 const Search = () => {
   const handleChange = (event) => {
