424c009b0c6b0b8d6ee1c663fb4ec126129a71b3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-30 11:07:21 -0500

    add <p> to display search term entered in input field. Add searchTerm useState hook to Search component and setSearchTerm state updater function to handleChange function.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,10 @@ const App = () => {
 };
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
   const handleChange = (event) => {
+    setSearchTerm(event.target.value);
     console.log(event.target.value);
   };
 
@@ -43,6 +46,10 @@ const Search = () => {
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
     </div>
   ); 
 };
