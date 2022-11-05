5427930cb68d5cd69a8197ac7dc239cca35d6640
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-05 14:07:08 -0600

    add searchTerm useState to App component and setSearchTerm to handleSearch function. Remove onHandleSearch function from Search component and added onSearchProp to onChange handler. - Lifting State to App so search and list both have access to searchTerm. - WIP

--- src/App.js
+++ src/App.js
@@ -20,8 +20,10 @@ const App = () => {
     }
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleSearch = (event) => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
   
   return(
@@ -38,23 +40,14 @@ const App = () => {
 };
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    console.log(event.target.value);
-
-    props.onSearch(event);
-  };
-
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
+      <input id="search" type="text" onChange={props.onSearch} />
+      {/* 
       <p>
         Searching for <strong>{searchTerm}</strong>.
-      </p>
+      </p> */}
     </div>
   ); 
 };
