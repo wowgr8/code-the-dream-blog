66403fc5d28195e1295a833b7c63ee331b57c1e1
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-05 14:31:38 -0600

    add refactored searchedStories function - concise body and implicit return.

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter(function (story) {
-    return story.title.includes(searchTerm);
-  });
+  const searchedStories = stories.filter((story) => 
+  story.title.includes(searchTerm)
+  );
   
   return(
     <div>
