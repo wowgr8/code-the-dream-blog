0636f4f5fa20fd36682e0011edcdb72b7cbda4d1
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-10 23:13:05 -0800

    add refactored props - destructured props in Item function component signature.

--- src/App.js
+++ src/App.js
@@ -63,14 +63,14 @@ const List = ({ list }) => (
   </ul>
 ); 
 
-const Item = (props) => (
+const Item = ( {item} ) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li> 
 );
 
