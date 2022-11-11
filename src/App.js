5fe4a823757ecf4a86c7a401a6fe240c9d372691
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-10 23:10:04 -0800

    add refactored component body from block back to concise. Add destructured props directly in function signature.

--- src/App.js
+++ src/App.js
@@ -43,21 +43,17 @@ const App = () => {
   )
 };
 
-const Search = (props) => {
-  const { search, onSearch } = props;
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input 
-        id="search"
-        type="text" 
-        value={search} 
-        onChange={onSearch} 
-      />
-    </div>
-  )
-}; 
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input 
+      id="search"
+      type="text" 
+      value={search} 
+      onChange={onSearch} 
+    />
+  </div>
+); 
 
 const List = (props) => (
   <ul>
