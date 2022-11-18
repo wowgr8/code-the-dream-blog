cdcdc2f6b6ae514c660be28cb312a58f0579d46e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-17 19:24:36 -0800

    add refactored props in List function component - replace individual props with spread operator

--- src/App.js
+++ src/App.js
@@ -58,14 +58,7 @@ const Search = ({ search, onSearch }) => (
 const List = ({ list }) => (
   <ul>
     {list.map((item) => (
-      <Item 
-        key={item.objectID} 
-        title={item.title} 
-        url={item.url}
-        author={item.author}
-        num_comments={item.num_comments}
-        points={item.points}
-      /> 
+      <Item key={item.objectID} {...item}/> 
     ))}
   </ul>
 ); 
