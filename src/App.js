e0474a89d09559b1337f4222bab68ae0bdc1c815
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-22 13:53:13 -0800

    add custom hook to synchronize the state with the browser’s local storage - useSemiPersistentState(), moved previous useState and useEffect into it.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+const useSemiPersistentState = () => {
+  const [searchTerm, setSearchTerm] = useState(localStorage.setItem('search') || ''); //  use the stored value, if a value exists, to set the initial state of the searchTerm in React’s useState Hook. Otherwise, default to our initial state as before
+
+  useEffect(() => { 
+    localStorage.setItem('search', searchTerm); // uses local storage to store the searchTerm accompanied by an identifier whenever a user types into the HTML input field
+  }, [searchTerm]); // Whenever and wherever the searchTerm state is updated via setSearchTerm, the browser’s local storage will always be in sync with it.
+}
 
 const App = () => {
   const stories = [
@@ -20,13 +28,7 @@ const App = () => {
     }
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || 'React'            //  use the stored value, if a value exists, to set the initial state of the searchTerm in React’s useState Hook. Otherwise, default to our initial state as before
-  );  
-  
-  useEffect(() => { 
-    localStorage.setItem('search', searchTerm); // uses local storage to store the searchTerm accompanied by an identifier whenever a user types into the HTML input field
-  }, [searchTerm]); // Whenever and wherever the searchTerm state is updated via setSearchTerm, the browser’s local storage will always be in sync with it.
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('React');  
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
