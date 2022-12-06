9993071e9973f8f0b536864011f461925040bfb2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-12-06 13:27:48 -0800

    rename stories array to initialStories array and move it top as global variable. Create stories useState hook with initialStories array as initial state.

--- src/App.js
+++ src/App.js
@@ -10,30 +10,33 @@ const useSemiPersistentState = (key, initialState) => { // synchronizes the stat
   return [value, setValue];
 }
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    }, 
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ];
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     'search',
     'React'
   );  
+
+  const [stories, setStories] = useState(initialStories);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
