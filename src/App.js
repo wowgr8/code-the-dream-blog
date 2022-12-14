62bb0251a1caf1b55a48b30c27881ca2c9b5eb08
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-12-13 17:24:21 -0800

    remove shorthand version for the promise to mimic API call delay.

--- src/App.js
+++ src/App.js
@@ -30,7 +30,10 @@ const initialStories = [
 ];
 
 const getAsyncStories = () => // returns a promise with data in its shorthand version once it resolves. The resolved object holds the previous list of stories
-  Promise.resolve({ data: { stories: initialStories } });
+  new Promise((resolve) =>
+    resolve({ data: { stories: initialStories } })
+  );
+  //Promise.resolve({ data: { stories: initialStories } });
 
 const App = () => {
 
