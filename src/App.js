e643e2a09906d3d39646460bf7a753ef7e2337bd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-15 17:04:01 -0800

    add re-structured API GET request to use Axios syntax.

--- src/App.js
+++ src/App.js
@@ -66,12 +66,12 @@ const App = () => {
   const handleFetchStories = useCallback(() => { // Memoized handler example
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         }); 
       })
       .catch(() =>
