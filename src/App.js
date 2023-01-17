8c73dfdab36cbd7fc44c43732e0720bfaf26d53f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-16 18:09:39 -0800

    add try catch error handling to new async/await api call.

--- src/App.js
+++ src/App.js
@@ -66,12 +66,17 @@ const App = () => {
   const handleFetchStories = useCallback( async () => { // Memoized handler example
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    const result = await axios.get(url);
+    try {
+      const result = await axios.get(url);
 
-    dispatchStories({
-      type: 'STORIES_FETCH_SUCCESS',
-      payload: result.data.hits,
-    }); 
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      }); 
+      
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
   }, [url]); 
 
   useEffect(() => {
